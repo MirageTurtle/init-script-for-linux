8e57d7e9aa54f7adc2b845fc334e8029b24783f1
MirageTurtle <mirageturtle@gmail.com>
2023-04-13 14:23:38 +0800

    (update)Add shebang and oh-my-zsh plugin part in zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
+
 # TODO:
 # 1. using $SHELL to check the default shell is not credible.
 # 2. if the zsh is installed, the install should be passed.
+# 3. extract the requirements into a function, curl/wget, git, ...
 
-if ! [[ `echo $SHELL` =~ (\/usr)?\/bin\/zsh ]]
+if [[ ! `echo $SHELL` =~ (/usr)?/bin/zsh ]]
 then
     echo "The default shell is $(echo $SHELL), will config default shell to zsh"
     sudo apt install -y zsh
@@ -22,4 +25,13 @@ then
     else
 	echo "Please install curl or wget first."
     fi
+fi
+
+if command -v git > /dev/null 2>&1
+then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sed -i /^"plugins=("/s/")"/" zsh-syntax-highlighting zsh-autosuggestions)"/ ~/.zshrc
+else
+    echo "Please install git first."
 fi
