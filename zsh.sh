6b5532136f827f138dcfe73b68bce4bae55bd671
MirageTurtle <mirageturtle@gmail.com>
2023-06-30 22:26:56 +0800

    (zsh.sh)Add powerlevel10k support.

--- zsh.sh
+++ zsh.sh
@@ -5,17 +5,17 @@
 # 2. if the zsh is installed, the install should be passed.
 # 3. extract the requirements into a function, curl/wget, git, ...
 
-if [[ ! `echo $SHELL` =~ (/usr)?/bin/zsh ]]
-then
+if [[ ! `echo $SHELL` =~ (/usr)?/bin/zsh ]]; then
     echo "The default shell is $(echo $SHELL), will config default shell to zsh"
     sudo apt install -y zsh
-    chsh
+    echo "/usr/bin/zsh" > chsh
 fi
 
 ohmyzsh_folder="${HOME}/.oh-my-zsh/"
-if [ ! -d "$ohmyzsh_folder" ]
-then
-    echo "no oh-my-zsh, will download and install oh-my-zsh."
+if [[ -d "$ohmyzsh_folder" ]]; then
+    echo "There have been Oh-my-zsh and the installation is passed."
+else
+    echo "There's no Oh-my-zsh, will download and install oh-my-zsh."
     if command -v curl > /dev/null 2>&1
     then
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -23,16 +23,32 @@ then
     then
 	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     else
-	echo "Please install curl or wget first."
+	echo "[ERROR]Please install curl or wget first."
+	exit 1
     fi
+    echo "Oh-my-zsh installed."
 fi
 
-if command -v git > /dev/null 2>&1
-then
+# Oh-my-zsh plug-ins
+echo "Config Oh-my-zsh plug-ins..."
+if command -v git > /dev/null 2>&1; then
     git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-    sed -i /^"plugins=("/s/")"/" zsh-syntax-highlighting zsh-autosuggestions fzf-zsh-plugin)"/ ~/.zshrc
+    sed -i /"^plugins=("/s/")"/" zsh-syntax-highlighting zsh-autosuggestions fzf-zsh-plugin)"/ ~/.zshrc
 else
-    echo "Please install git first."
+    echo "[ERROR]Please install git first."
+    exit 1
 fi
+echo "Plug-ins configuration finished."
+
+# Oh-my-zsh theme
+echo "Config Oh-my-zsh theme into powerlevel10k..."
+if command -v git > /dev/null 2>&1; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    sed -i /'^ZSH_THEME="'/s/'^ZSH_THEME=".*"$'/'ZSH_THEME="powerlevel10k\/powerlevel10bk"'/ ~/.zshrc
+else
+    echo "[ERROR]Please install git first."
+    exit 1
+fi
+echo "powerlevel10k configuration finished."
